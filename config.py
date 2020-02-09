d16bd28d5196c20cf1efe867d8371ef955826e6d
Wanjiku Karanja <ciiku92@gmail.com>
2020-02-10 01:34:52 +0300

    Create General Config, ProdConfig and DevConfig classes

---
+++ config.py
@@ -0,0 +1,31 @@
+import os
+
+class Config:
+    """
+    General configurations' parent class
+    """
+
+    SECRET_KEY = 'wanjiku'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://ciku:1234@localhost/pitches'
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+
+
+class ProdConfig(Config):
+    """
+    Production configuration child class
+    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    """
+    Development configuration child class
+    """
+
+
+    DEBUG = True
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
