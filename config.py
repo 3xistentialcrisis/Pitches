7aac0ce5e90d50c80eaf7ee9790a2b5d7a35c98a
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-07 23:28:26 +0300

    Turn off Flask-SQLAlchemy event system & disable SQLALCHEMY_TRACK_MODIFICATIONS warning

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ load_dotenv()
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     print(SQLALCHEMY_DATABASE_URI)
     Debug=True
