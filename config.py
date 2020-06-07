e0bdf50b9995c8084d49dcc4b5ef2d236628557d
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-07 23:09:57 +0300

    Alter config file to install dotenv

--- config.py
+++ config.py
@@ -1,18 +1,9 @@
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 class Config:
-    """
-    General configuration class
-    """
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-
-    class ProdConfig(Config):
-        pass
-
-    class DevConfig(Config):
-        DEBUG = True
-
-    config_options = {
-        'development': DevConfig,
-        'production': ProdConfig
+    print(SQLALCHEMY_DATABASE_URI)
+    Debug=True
