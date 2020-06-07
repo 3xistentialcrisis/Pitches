b27c8046e50444f2cc671325bf3161dbadaa08c2
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-07 23:02:38 +0300

    Create database migration

--- manage.py
+++ manage.py
@@ -1,11 +1,14 @@
 from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
 from app import create_app
 from app.models import User
 
 app = create_app()
 
 manager = Manager(app)
+migrate = Migrate(app,db
 manager.add_command('run',Server(use_debugger=True))
+manager.add_command('db',MigrateCommand)
 
 # Create Flask-script shell
 @manager.shell
