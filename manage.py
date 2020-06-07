05a18831b1b74ebf3751806590e680cd596b7275
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-07 23:16:46 +0300

    Define 'db' name in migrate class

--- manage.py
+++ manage.py
@@ -1,12 +1,14 @@
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
-from app import create_app
+from app import create_app, db
 from app.models import User
 
 app = create_app()
 
 manager = Manager(app)
-migrate = Migrate(app,db
+
+# Initialise migrate class
+migrate = Migrate(app,db)
 manager.add_command('run',Server(use_debugger=True))
 manager.add_command('db',MigrateCommand)
 
