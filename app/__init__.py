4619a39ec9f283d68993bc15d39f7f154c15018a
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-07 23:14:40 +0300

    Correct authentication_blueprint folder to main

---
+++ app/__init__.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+
+def create_app():
+
+    # Create app configurations
+    app.config.from_object(Config)
+
+    # Registering blueprints
+    from .main import main as authentication_blueprint
+    from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
+    app.register_blueprint(main_blueprint)
+
+    db.init_app(app)
+
+    return app
+
+
+
