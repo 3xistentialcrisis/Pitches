42961c5f73dd6d16116b430bf85abdf885591167
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-09 00:22:48 +0300

    Create module to add new pitch

--- app/main/form.py
+++ app/main/form.py
@@ -1,7 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    bio = TextAreaField('Write a few biographical facts about yourself.',validators = [Required()])
     submit = SubmitField('Save')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
+    category = SelectField('Category', choices=[('Mystery','Mystery'),('Thriller','Thriller'),('Romance','Romance')],validators=[Required()])
+    submit = SubmitField('Post')
