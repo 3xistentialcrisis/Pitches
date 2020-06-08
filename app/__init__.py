a410092455528b42c64bf430371500253b1a98b5
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-08 19:48:43 +0300

    Import and instatiate Login Manager Class

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from config import Config
+from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
+bootstrap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.load_view = 'auth.login'
 app = Flask(__name__)
+
 
 
 def create_app():
@@ -17,6 +24,9 @@ def create_app():
     app.register_blueprint(authentication_blueprint,url_prefix = '/authenticate')
     app.register_blueprint(main_blueprint)
 
+    # Initialise Flask Extensions
+    login_manager.init_app(app)
     db.init_app(app)
+    bootstrap.init_app(app)
 
     return app
