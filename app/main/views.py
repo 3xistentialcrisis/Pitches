b699c1dbedc889772c0c016fb491d313aa2daf44
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-11 14:02:53 +0300

    Correct upvote view

--- app/main/views.py
+++ app/main/views.py
@@ -10,7 +10,7 @@ from .. import db,photos
 def index():
     pitches = Pitch.query.all()
     mystery = Pitch.query.filter_by(category='mystery').all()
-    return render_template('index.html', mystery=mystery, pitches=pitches)
+    # return render_template('index.html', mystery=mystery, pitches=pitches)
     thriller = Pitch.query.filter_by(category='thriller').all()
     romance = Pitch.query.filter_by(category='romance').all()
     return render_template('index.html', mystery=mystery, thriller=thriller, pitches=pitches, romance=romance)
@@ -41,7 +41,6 @@ def comment(pitch_id):
         pitch_id = pitch_id
         user_id = current_user._get_current_object().id
         new_comment = Comment(comment = comment,user_id = user_id,pitch_id = pitch_id)
-
         new_comment.save_c()
         return redirect(url_for('.comment', pitch_id = pitch_id))
     return render_template('comment.html', form=form, pitch = pitch, all_comments=all_comments)
@@ -85,12 +84,18 @@ def update_pic(name):
 @main.route('/like/<int:id>',methods = ['POST','GET'])
 @login_required
 def like(id):
-    user = Upvote.query.get(id)
-    all_upvotes = Upvote.query.filter_by(id=id).all()
-    pitch = Pitch.query.get(id)
-    new_vote = Upvote(user = user, pitch = pitch,upvote = 1)
+    get_pitches = Upvote.get_upvotes(id)
+    valid_string = f'{current_user.id}:{id}'
+    for pitch in get_pitches:
+        to_str = f'{pitch}'
+        print(valid_string+" "+to_str)
+        if valid_string == to_str:
+            return redirect(url_for('main.index',id=id))
+        else:
+            continue
+    new_vote = Upvote(user = current_user, pitch_id=id)
     new_vote.save()
-    return redirect(url_for('main.index',id = pitch.id))
+    return redirect(url_for('main.index',id=id))
 
 @main.route('/dislike/<int:id>',methods = ['POST','GET'])
 @login_required
