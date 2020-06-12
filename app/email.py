99bdeabc014817157ba6df6075e57e68d4ee8dc0
Wanjiku Karanja <ciiku92@gmail.com>
2020-06-12 14:49:23 +0300

    Deploy app

--- app/email.py
+++ app/email.py
@@ -2,7 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
-def mail_message(subject,template,to,**kwargs):
+def mail_message(subject: object, template: object, to: object, kwargs: object) -> object:
     sender_email = 'ciiku92@gmail.com'
     email = Message(subject, sender=sender_email, recipients=[to])
     email.body= render_template(template + ".txt",**kwargs)
